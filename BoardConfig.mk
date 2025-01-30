3ebc92cf75da4a58c70b56b7ace56155522d5daa
xenxynon <flynryder427@gmail.com>
2025-01-30 21:14:21 +0530

    Spacewar: do not enforce fcm
    Change-Id: I7c3dab97ceed5cf4b098359065bc99126c4950bd
    Signed-off-by: xenxynon <flynryder427@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -105,11 +105,11 @@ TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/config.fs
 TARGET_SURFACEFLINGER_UDFPS_LIB := //hardware/nothing:libudfps_extension.nothing
 
 # HIDL
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
     $(DEVICE_PATH)/configs/vintf/vendor_framework_compatibility_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     vendor/yaap/config/device_framework_matrix.xml
-DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
+DEVICE_MATRIX_FILE += hardware/qcom-caf/common/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/configs/vintf/manifest.xml
 
 # Lineage Health
